2a78f309d6657a5e33338b20180f428e5af95115
amarianjel <abrm.enginner@gmail.com>
2024-03-04 18:10:20 -0300

    email service

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -27,10 +27,12 @@ export class AuthService {
 
       const { password, ...userEntity } = UserEntity.fromObject(user);
 
+      const token = await JwtAdapter.generateToken({ id: user.id, email: user.email });
+      if ( !token ) throw CustomError.internalServer('Error while creating JWT');
 
       return { 
         user: userEntity, 
-        token: 'ABC' 
+        token: token 
       };
 
     } catch (error) {
